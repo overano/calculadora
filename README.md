704634b70ad53d4d12063df5759ece56baea35ff
Oscar Verano <overano@atsistemas.com>
2023-02-23 15:38:45 +0100

    added logic from library

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Install jar locally
+# Install library jar locally
 ```
 mvn install:install-file \
-   -Dfile=<path-to-file> \
+   -Dfile=<path-to-jar-file> \
    -DgroupId=io.corp.calculator \
    -DartifactId=tracer \
    -Dversion=1.0.0 \
@@ -9,3 +9,18 @@ mvn install:install-file \
    -DgeneratePom=true
 
 ```
+
+# Build jar
+```sh
+$ mvn clean package
+```
+
+# Run Jar
+```sh
+$ java -jar target/calculadora-0.0.1-SNAPSHOT.jar
+```
+
+# uri path to get sum
+> http://localhost:8080/sum/{param1}/{param2}
+
+### Param1
--- src/main/java/com/company/calculadora/controller/CalcController.java
+++ src/main/java/com/company/calculadora/controller/CalcController.java
@@ -1,6 +1,7 @@
 package com.company.calculadora.controller;
 
 import com.company.calculadora.service.CalcService;
+import io.corp.calculator.TracerImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalcController {
 
     private final CalcService service;
+    private TracerImpl tracer = new TracerImpl();
 
     @GetMapping("/sum/{param1}/{param2}")
     public Integer getSum(@PathVariable("param1") Integer param1, @PathVariable("param2")  Integer param2){
-        return service.getSum(param1, param2);
+
+        Integer result = service.getSum(param1, param2);
+
+        tracer.trace(result);
+
+        return result;
     }
 
 }
